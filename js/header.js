b5dec76439e1bb2d9733a7055a67b993f919fa6d
rob dev <robthewebdev@gmail.com>
2018-03-21 15:55:27 -0500

    new code

--- js/header.js
+++ js/header.js
@@ -1,18 +1,30 @@
 // put event handlers for header links here
-$(document).ready(function() {
-    var $modal = $("[rel=js-modal]");
+var H = (function () {  
 
-    $("[rel=js-controls]").on("click", "[rel*='js-']", function(evt){
-        evt.preventDefault();
-        evt.stopPropagation();
-        
-        var url = $(evt.target).attr("href");
+    function headerLinkClicks(evt) {  
+            evt.preventDefault();
+            evt.stopPropagation();
+            
+            var url = $(evt.target).attr("href");
 
-        $.ajax(url, {dataType: "text"})
-        .then(function (contents){
-            $modal.html(contents).show();
-        });
-        
-    });
+            $.ajax(url, {dataType: "text"})
+            .then(function (contents){
+                $modal.html(contents).show();
+            });
+        }
 
-});
+    function init() {
+        var $modal = $("[rel=js-modal]");
+
+        $("[rel=js-controls]").on("click", "[rel*='js-']", headerLinkClicks);
+    }
+
+    var $modal;
+
+    return {
+        init: init
+    }
+       
+})();
+
+$(document).ready(H.init);
